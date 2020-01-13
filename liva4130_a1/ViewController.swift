87a14d4ba0b762497800cac71e0d511052439d9a
FranchescoLivado <livadofranchesco@gmail.com>
2020-01-13 14:47:13 -0500

    initial commit

---
+++ liva4130_a1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  liva4130_a1
+//
+//  Created by Franchesco Livado on 2020-01-13.
+//  Copyright © 2020 wlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var likeButton: UILabel!
+    @IBOutlet weak var dislikeButton: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func like(_ sender: Any) {
+    }
+    
+    
+    @IBAction func dislike(_ sender: Any) {
+    }
+    
+    @IBAction func next(_ sender: Any) {
+    }
+}
+
